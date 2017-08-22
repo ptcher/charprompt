1e2175e0e02a2307e6308ab8f59e5ffd48ec7012
Philip Tchernavskij <ptchernavskij@protonmail.com>
2017-08-23 00:07:48 +0200

    Adjust amount of prompts.

--- sug.py
+++ sug.py
@@ -3,24 +3,24 @@ import random
 
 
 def sampleprint(P,k=5):
-    for i in range(5):
+    for _ in range(k):
         p = random.choice(P)
         print " ".join([";".join([x.name() for x in p.lemmas()]), ":", p.definition()])
 
 def readin(infile):
     return [line.rstrip() for line in open(infile).readlines()]
 
-P = []
+professions = []
 for ss in wn.all_synsets("n"): 
     if ss.lexname() == "noun.person" and str(ss.definition).count("inhabit") < 1 and str(ss.definition).count("living") < 1 and len(ss.instance_hypernyms()) < 1:
-        P.append(ss)
-sampleprint(P)
+        professions.append(ss)
+sampleprint(professions, 1)
 
 
-P = []
+adjectives = []
 for ss in wn.all_synsets("a"):
-    P.append(ss)
-sampleprint(P)
+    adjectives.append(ss)
+sampleprint(adjectives, 3)
 
 
 instincts = readin("instincts.txt")
