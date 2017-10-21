bc718b504431f8ead7cbc8bb86ab24f894b3fbbc
Philip Tchernavskij <ptchernavskij@protonmail.com>
2017-10-21 18:08:21 +0200

    Set up an input loop to avoid several-second startup time for every prompt.

--- charprompt.py
+++ charprompt.py
@@ -1,6 +1,8 @@
 from nltk.corpus import wordnet as wn
 import random
 
+# TODO maybe write large sample sets to files (if reading from file is faster than getting them out of wordnet)
+
 def sampleprint(P,k=5):
     for _ in range(k):
         p = random.choice(P)
@@ -22,7 +24,15 @@ adjectives = list(wn.all_synsets("a"))
 instincts = readin("instincts.txt")
 knacks = readin("knacks.txt")
 
-sampleprint(professions, 1)
-sampleprint(adjectives, 3)
-print "Instinct: %s" % random.choice(instincts)
-print "Knack: %s" % random.choice(knacks)
+while True:
+    try:
+        raw_input('Push button receive prompt')
+        print ""
+        sampleprint(professions, 1)
+        sampleprint(adjectives, 3)
+        print "Instinct: %s" % random.choice(instincts) 
+        print "Knack: %s" % random.choice(knacks) 
+        print ""
+    except:
+        print "\nBye!"
+        break
